65cb79f769e02b48560077a10fb9bd6fac74d989
joosbeen <joosbeen@gmail.com>
2023-09-20 19:54:08 -0600

    config security v2

--- gateway-service/src/main/java/com/microservices/gatewayservice/config/AuthFilter.java
+++ gateway-service/src/main/java/com/microservices/gatewayservice/config/AuthFilter.java
@@ -1,28 +1,29 @@
 package com.microservices.gatewayservice.config;
 
+import com.microservices.gatewayservice.dto.TokenDto;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.server.ServerWebExchange;
-
-import com.microservices.gatewayservice.dto.TokenDto;
 
 import reactor.core.publisher.Mono;
 
 @Component
 public class AuthFilter extends AbstractGatewayFilterFactory<AuthFilter.Config> {
 	
+	@Autowired
 	private WebClient.Builder webClient;
 	
-	
-	
-	public AuthFilter(WebClient.Builder webClient) {
+	/*public AuthFilter(WebClient.Builder webClient) {
 		super(Config.class);
 		this.webClient = webClient;
-	}
+	}*/
 
 	@Override
 	public GatewayFilter apply(Config config) {
@@ -46,7 +47,7 @@ public class AuthFilter extends AbstractGatewayFilterFactory<AuthFilter.Config> 
 	}
 	
 	public Mono<Void> onError(ServerWebExchange exchange, HttpStatus status) {
-		var response = exchange.getResponse();
+		ServerHttpResponse response = exchange.getResponse();
 		response.setStatusCode(status);
 		return response.setComplete();
 	}
